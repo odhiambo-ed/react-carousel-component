dfc20d1952396cfe0d28869ae8b7e2ac0a9d1c98
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-03 11:37:27 +0300

    edit app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
+import "./App.css";
 import SliderComponent from "./components/SliderComponent";
 
 function App() {
-
   return (
     <>
       <SliderComponent
         url={"https://picsum.photos/v2/list"}
-        page={"1"}
-        limit={"10"}
+        page={2}
+        limit={10}
       />
     </>
   );
 }
 
-export default App
+export default App;
